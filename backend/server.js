e349f6dd28490aa1fa32f2228197a7c1635627a7
Angelito607 <angelitoguapoxd@gmail.com>
2026-07-15 22:58:31 -0600

    Add explicit CORS headers for backend

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,13 @@ app.use(cors({
 
 app.options('*', cors());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
+
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
